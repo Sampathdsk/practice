8757bb14a8d5d59bf45d6e0222478c1f58f88cb9
Sampathdsk <93008686+Sampathdsk@users.noreply.github.com>
2021-10-27 03:14:00 +0530

    Add files via upload

---
+++ greatest.java.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int a=100;
+		int b=30;
+		int c=90;
+		if(a>b && a>c)
+			System.out.println("a is greater ="+ a);
+		else if(b>c)
+			System.out.println("b is greater ="+ b);
+		else
+			System.out.println("c is greater ="+ c);
+	}
+
+}
